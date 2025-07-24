309d60e8ea20a7bc50050d0a60b1848c54d3b250
quan <quan93909@gmail.com>
2025-07-24 08:02:06 +0700

    5.1 Làm giao diện
    Bài 41

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /admin/songs
 export const index = async (req: Request, res: Response) => {
@@ -10,5 +12,25 @@ export const index = async (req: Request, res: Response) => {
   res.render("admin/pages/songs/index", {
     pageTitle: "Quan ly bai hat",
     songs: songs
+  });
+}
+
+// [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    status: "active",
+    deleted: false
+  }).select("title");
+
+  const singers = await Singer.find({
+    status: "active",
+    deleted: false
+  }).select("fullName");
+
+
+  res.render("admin/pages/songs/create", {
+    pageTitle: "Them moi bai hat",
+    topics: topics,
+    singers: singers
   });
 }
